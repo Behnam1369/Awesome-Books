9d13812f5114c36bda0f0edc6a34d8320532d24b
Behnam1369 <behnam.aghaali@yahoo.com>
2022-02-14 22:15:16 +0330

    Recreate project using modules

---
+++ modules/navigator.js
@@ -0,0 +1,25 @@
+export default class Navigator {
+  constructor() {
+    this.links = [];
+    this.activeSection = '#list';
+  }
+
+  navigate(x) {
+    const lastActiveSection = document.querySelector(this.activeSection);
+    lastActiveSection.classList.remove('active');
+    this.activeSection = x;
+    const section = document.querySelector(x);
+    section.classList.add('active');
+  }
+
+  initiate() {
+    window.addEventListener('popstate', () => {
+      this.links = Array.from(document.querySelectorAll('nav a '));
+      this.links.filter((el) => el.href !== window.location.href).forEach((el) => {
+        el.classList.remove('active');
+      });
+      this.links.filter((el) => el.href === window.location.href)[0].classList.add('active');
+      this.navigate(window.location.hash);
+    });
+  }
+}
